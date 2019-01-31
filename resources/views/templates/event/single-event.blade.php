adc72f0e48f59176c0c3ac14279921fede8b26f7
Tim Albert <timothy.m.albert@gmail.com>
2019-01-31 12:19:00 -0500

    Streamline events page

--- resources/views/templates/event/single-event.blade.php
+++ resources/views/templates/event/single-event.blade.php
@@ -1,23 +1,7 @@
-@php
-$start_date = tribe_get_start_date(get_the_id(), false, 'F j, Y');
-$start_time = tribe_get_start_date(get_the_id(), false, 'g:i a');
-$end_date = tribe_get_end_date(get_the_id(), false, 'F j, Y');
-$end_time = tribe_get_end_date(get_the_id(), false, 'g:i a');
-
-if (tribe_event_is_multiday()) {
-  $event_date = $start_date .' &mdash; '. $end_date;
-} elseif (tribe_event_is_all_day()) {
-  $event_date = $start_date .'<br>All Day';
-} else {
-  $event_date = $start_date .'<br>'. $start_time .' - '. $end_time;
-}
-@endphp
-
 @while(have_posts()) @php(the_post())
   <div class="row">
     <div class="col w--1/2@d">
       {{ tribe_the_notices() }}
-
       {!! the_content() !!}
 
       @if(tribe_get_event_website_url())
@@ -27,8 +11,8 @@ if (tribe_event_is_multiday()) {
     <div class="col w--1/4@d">
       <div class="card h--auto mb--1">
         <div class="__body pb--1">
-          <div class="__title mb--1">{!! $event_date !!}</div>
-          <div><a class="btn --sm" href="?ical"><i data-feather="calendar"></i> iCal</a></div>
+          <div class="__title mb--1">{!! \Kernl\Utility::getTribeDate(get_the_id()) !!}</div>
+          <div class="mb--1"><a class="btn --sm" href="?ical"><i data-feather="calendar"></i> iCal</a></div>
           <p>{!! '<b>' . tribe_get_venue() . '</b>' . (tribe_address_exists() ? '<br>'. tribe_get_full_address() : '') !!}</p>
           {!! tribe_get_event_categories(null, ['label_before' => '<div class="tt--u">', 'wrap_before' => '<ul class="fs--sm">']) !!}
         </div>
