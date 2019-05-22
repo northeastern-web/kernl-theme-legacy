c29561be02b428399c155caf881b3103b7f43f51
Tim Albert <timothy.m.albert@gmail.com>
2019-05-22 15:25:52 -0400

    Update profile component templates with modal condition

--- resources/views/templates/profile/media.blade.php
+++ resources/views/templates/profile/media.blade.php
@@ -1,5 +1,9 @@
-<div class="media">
-  <a class="__link" href="#" data-toggle="modal" data-target="#modal_profile-{{ get_the_ID() }}">
+@php
+  $hasmodal = (get_the_content() || get_field('txt_email') || get_field('txt_phone') ? true : false);
+@endphp
+
+<div class="media {{ (isset($class) ? $class : '') }}">
+  <a class="__link" href="#" {!! ($hasmodal ? 'data-toggle="modal" data-target="#modal_profile-'. get_the_ID() .'"' : '') !!}>
     @if(get_field('med_headshot'))
       <div class="__graphic">
         <img class="__graphic__img" src="{{ get_field('med_headshot')['url'] }}" alt="">
@@ -7,19 +11,21 @@
     @endif
 
     <div class="__body">
-      <h4 class="__title">{{ the_field('txt_fname') }} {{ the_field('txt_lname') }}</h4>
+      <h5 class="__title">{{ the_field('txt_fname') }} {{ the_field('txt_lname') }}</h5>
 
       @if(get_field('txt_title'))
-        <div><i>{{ the_field('txt_title') }}</i></div>
+        <div>{{ the_field('txt_title') }}</div>
       @endif
 
       @if(get_field('txt_email'))
-        <div class="fs--sm">{{ the_field('txt_email') }}</div>
+        <div class="fs--xs pt--0h">{{ the_field('txt_email') }}</div>
       @endif
     </div>
   </a>
 </div>
 
 @php
-  include \App\template_path(locate_template('views/templates/profile/modal.blade.php'));
+  if($hasmodal) {
+    include \App\template_path(locate_template('views/templates/profile/modal.blade.php'));
+  }
 @endphp
