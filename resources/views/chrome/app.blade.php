cc73cde21c3d73f5fa90bf328504ca8872835e62
Tim Albert <timothy.m.albert@gmail.com>
2019-05-23 11:18:58 -0400

    Streamline template actions

--- resources/views/chrome/app.blade.php
+++ resources/views/chrome/app.blade.php
@@ -1,14 +1,8 @@
 <!doctype html>
 <html @php(language_attributes()) @php(body_class()) id="html">
   @include('chrome._head')
-  <body {!! (\Kernl\Lib\Utility::getGlobalContain() ? 'class="'. \Kernl\Lib\Utility::getGlobalContain() .'"' : '') !!}>
-    {!! (\WP_ENV == 'production' ? \Kernl\Lib\NU::gtmNoScript() : '<!-- GTM No Script tag -->') !!}
-    <a class="skip alert" href="#main_content">Skip to main content</a>
-    <!--[if IE]>
-      <div class="bg--beige fs--sm pa--1 pa--2@d">
-        <b><i>Note</i></b>: You are using an <strong>outdated</strong> browser. Please <a class="tc--red" href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.
-      </div>
-    <![endif]-->
+  <body {!! (\Kernl\Lib\Utility::getGlobalContain()) !!}>
+    {{ wp_body_open() }}
 
     @php(do_action('get_header'))
 
@@ -26,12 +20,5 @@
     @include('templates.search._modal')
 
     @php(wp_footer())
-    @if(get_field('bool_chrome_header', 'option') || get_field('bool_chrome_footer', 'option'))
-      <script src="{{ \Kernl\Lib\NU::chromeScript() }}"></script>
-    @endif
-
-    @if(\WP_ENV == 'production' && get_field('txt_analytics', 'option'))
-      {!! \Kernl\Lib\NU::googleAnalytics(get_field('txt_analytics', 'option')) !!}
-    @endif
   </body>
 </html>
