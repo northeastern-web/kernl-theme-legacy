ea95783dc2227ab14a17e08c0264ef399427ec19
Cameron Van Orman <cameron.vanorman@gmail.com>
2020-12-22 12:00:59 -0800

    Updating 404 page template with updated references to the theme's template paths

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,13 +1,16 @@
-@extends('layouts.app')
+@extends('chrome.app')
 
 @section('content')
-@include('partials.page-header')
-
-@if (! have_posts())
-<x-alert type="warning">
-  {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
-</x-alert>
-
-{!! get_search_form(false) !!}
-@endif
+@include('components.header')
+<article>
+  <div class="section">
+    @if (! have_posts())
+    <x-alert type="warning">
+      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
+    </x-alert>
+  
+    {!! get_search_form(false) !!}
+    @endif
+  </div>
+</article>
 @endsection
