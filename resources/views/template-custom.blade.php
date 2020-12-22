ee044a75bf35186c3465d5a3b3b1b1e1fb27fe5c
Cameron Van Orman <cameron.vanorman@gmail.com>
2020-12-22 14:37:27 -0800

    Updating layout reference and adding template._section and template._banner to bring in dynamic content from the page builder

--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -2,11 +2,15 @@
   Template Name: Custom Template
 --}}
 
-@extends('layouts.app')
+@extends('chrome.app')
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
-    @include('partials.content-page')
+  <article>
+      @include('templates._banner')
+      <div class="section">
+        @include('templates._section')
+      </div>
+    </article>
   @endwhile
 @endsection
