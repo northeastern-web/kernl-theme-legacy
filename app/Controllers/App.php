1adb80b234dc430d4357eca234756b622caaef18
Ray Kingston <r.kingston@northeastern.edu>
2021-08-05 21:43:48 -0400

    Release/1.3.16 (#2)
    * Update for primary category metaquery; version up
    
    * Edit for PR changes — function_exists and array index
    
    * Updating yarn.lock
    
    Authored-by: Ray Kingston <r.kingston@neu.edu>

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -64,9 +64,19 @@ class App extends Controller
         }
 
         if (is_single()) {
-            return (get_the_category() ?
+            if (function_exists('the_seo_framework')) {
+                $primary_cat = the_seo_framework()->get_primary_term_id( get_the_ID(), 'category' );
+            } else {
+                $primary_cat = null;
+            }
+
+            if (isset($primary_cat)) {
+                return '<a href="'. get_category_link($primary_cat) .'">'. get_term($primary_cat)->name .'</a>';
+            } else {
+                return (get_the_category() ?
                 '<a href="'. get_category_link(get_the_category()[0]) .'">'. get_the_category()[0]->name .'</a>'
             : '');
+            }
         }
     }
 
